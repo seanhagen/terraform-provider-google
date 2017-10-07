6b7d47de627ca57d90f3895a8dc843e754d4acea
Sean Patrick Hagen <sean.hagen@gmail.com>
2017-10-07 15:37:23 -0700

    Start on actual verification resource

--- google/resource_site_verification.go
+++ google/resource_site_verification.go
@@ -1,49 +1,95 @@
 package google
 
-// import ()
+import (
+	"fmt"
+	"github.com/hashicorp/terraform/helper/schema"
+	"google.golang.org/api/siteverification/v1"
+	"log"
+	"strings"
+)
 
-// func resourceSiteVerification() *schema.Resource {
-// 	return &schema.Resource{
-// 		Create: resourceSiteVerificationCreate,
-// 		Read:   resourceSiteVerificationRead,
-// 		Delete: resourceSiteVerificationDelete,
+func resourceSiteVerification() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceSiteVerificationCreate,
+		Read:   resourceSiteVerificationRead,
+		Delete: resourceSiteVerificationDelete,
 
-// 		Schema: map[string]*schema.Schema{
-// 			"identifier": &schema.Schema{
-// 				Type:        schema.TypeString,
-// 				Required:    true,
-// 				Description: "Domain name to verfiy",
-// 				ForceNew:    true,
-// 			},
-// 			"method": &schema.Schema{
-// 				Type:         schema.TypeString,
-// 				Required:     true,
-// 				ForceNew:     true,
-// 				ValidateFunc: siteVerificationMethodValid,
-// 			},
-// 			"id": &schema.Schema{
-// 				Type:     schema.TypeString,
-// 				Computed: true,
-// 				ForceNew: true,
-// 			},
-// 		},
-// 	}
-// }
+		Schema: map[string]*schema.Schema{
+			"identifier": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "Domain name to verfiy",
+				ForceNew:    true,
+			},
+			"method": &schema.Schema{
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: siteVerificationMethodValid,
+			},
+			"id": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+				ForceNew: true,
+			},
+		},
+	}
+}
 
-// func resourceSiteVerificationCreate(d *schema.ResourceData, meta interface{}) error {
-// 	config := meta.(*Config)
+func resourceSiteVerificationCreate(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+	method := d.Get("method").(string)
 
-// 	return nil
-// }
+	verificationReq := siteverification.SiteVerificationWebResourceResource{
+		Site: &siteverification.SiteVerificationWebResourceResourceSite{
+			Identifier: d.Get("identifier").(string),
+		},
+	}
+	log.Printf("[DEBUG] Site Verification insert request: %#v", verificationReq)
 
-// func resourceSiteVerificationRead(d *schema.ResourceData, meta interface{}) error {
-// 	config := meta.(*Config)
+	resp, err := config.
+		clientSiteVerification.
+		WebResource.Insert(method, verificationReq).Do()
+	if err != nil {
+		return fmt.Errorf("Error requesting site verification: %#v", err)
+	}
+	log.Printf("[DEBUG] Site verification insert response: %#v", resp)
 
-// 	return nil
-// }
+	err := d.Set("id", resp.Id)
+	return err
+}
 
-// func resourceSiteVerificationDelete(d *schema.ResourceData, meta interface{}) error {
-// 	config := meta.(*Config)
+func resourceSiteVerificationRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
 
-// 	return nil
-// }
+	readReq := siteverification.SiteVerificationWebResourceResource{
+		Site: &siteverification.SiteVerificationWebResourceResourceSite{
+			Identifier: d.Get("identifier").(string),
+		},
+	}
+	// TODO logging
+	resp, err := config.
+		clientSiteVerification.
+		WebResource.Get(d.Get("id").(string)).Do()
+
+	// TODO logging
+	return nil
+}
+
+func resourceSiteVerificationDelete(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+
+	deleteReq := siteverification.SiteVerificationWebResourceResource{
+		Site: &siteverification.SiteVerificationWebResourceResourceSite{
+			Identifier: d.Get("identifier").(string),
+		},
+	}
+	// TODO logging
+
+	resp, err := config.
+		clientSiteVerification.
+		WebResource.Delete(d.Get("id").(string)).Do()
+	// TODO logging
+
+	return nil
+}
